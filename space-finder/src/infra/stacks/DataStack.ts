c703f8c619d73ac354a04e3438ffe1f5bd4faa40
Fernando MainPC <fersants.nando.ferreira@gmail.com>
2024-03-01 01:32:17 -0300

    lambda desenvolvida com typescript

--- space-finder/src/infra/stacks/DataStack.ts
+++ space-finder/src/infra/stacks/DataStack.ts
@@ -1,10 +1,25 @@
 import {Stack, StackProps} from 'aws-cdk-lib'
 import { Construct } from 'constructs';
+import { getSuffixFromStack } from './Utils';
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 
 
 export class DataStack extends Stack {
+
+    public readonly spacesTable: ITable
+
     constructor(scope: Construct, id: string, props?: StackProps){
         super(scope, id, props)
+
+        const suffix = getSuffixFromStack(this)
+
+        this.spacesTable = new Table(this, 'SpacesTable', {
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            },
+            tableName: `SpaceStack-${suffix}`
+        })
     }
 }
